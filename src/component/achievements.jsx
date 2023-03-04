efb73929fc8eb8c4da9ca4a7b13f8cb8ebe8de7e
adity1999 <adity.kumar@collegedekho.com>
2023-03-04 12:32:38 +0530

    ccc

--- src/component/achievements.jsx
+++ src/component/achievements.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 const Achievements = () => {
     return (
-        <div class="experience inverted" id="achievements">
-            <div class="title">
-                <h1 class="heading-1 heading-1--dark heading-1--m">ACHIEVEMENTS</h1>
-                <div class="underline title__underline">
-                    <div class="underline--1"></div>
-                    <div class="underline--2"></div>
+        <div className="experience inverted" id="achievements">
+            <div className="title">
+                <h1 className="heading-1 heading-1--dark heading-1--m">ACHIEVEMENTS</h1>
+                <div className="underline title__underline">
+                    <div className="underline--1"></div>
+                    <div className="underline--2"></div>
                 </div>
             </div>
-            <section class="content-container">
-                <div class="content-list">
-                    <h1 class="content-list__title">SIH Grand Finalist</h1>
-                    <h5 class="content-list__name">
-                        <a target="blank" href="https://www.marketinc.net/" class="content-list__name-link">Smart India Hackathon 2022 </a>
+            <section className="content-container">
+                <div className="content-list">
+                    <h1 className="content-list__title">SIH Grand Finalist</h1>
+                    <h5 className="content-list__name">
+                        <a target="blank" href="https://www.marketinc.net/" className="content-list__name-link">Smart India Hackathon 2022 </a>
                         - Grand Finalist
                     </h5>
-                    <div class="content-list__about">I along with my team from DCRUST were selected for Grand Finale which was held at Jai Bharath College of Management and Engineering Technology, Kerala. We worked under the Indian Knowledge System (IKS), All India Council for Technical Education (AICTE) problem statement Sanskrit voice to text and interface in Google Search Engine (AD976)..
+                    <div className="content-list__about">I along with my team from DCRUST were selected for Grand Finale which was held at Jai Bharath College of Management and Engineering Technology, Kerala. We worked under the Indian Knowledge System (IKS), All India Council for Technical Education (AICTE) problem statement Sanskrit voice to text and interface in Google Search Engine (AD976)..
                     </div>
-                    <div class="content-list__date">June 2022 - August 2022</div>
+                    <div className="content-list__date">June 2022 - August 2022</div>
                 </div>
 
             </section>
